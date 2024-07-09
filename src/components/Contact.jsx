a6baadc116b51923914f04032811d5430c352c40
Marko <markogidej@yahoo.com>
2024-07-09 15:29:20 -0400

    Contact added

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {        
+        return (
+            <div>
+                <h4>Contact</h4>
+                <p>LinkedIn: <a href="https://www.linkedin.com/in/marko-gidej-56562192/">Marko's LinkedIn</a></p>
+            </div>
+        )
+    }
+}
+
+export default Contact
