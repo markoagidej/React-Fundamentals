5ee2827e4b7a00b3b0df7e2c28532a3df8398e26
Marko <markogidej@yahoo.com>
2024-07-09 15:18:09 -0400

    Header made

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
 const Header = () => {
+
+    const containerStyle = {
+        position: 'relative',
+        textAlign: 'center'
+    }
+
+    const imageStyle = {
+        position: 'absolute',
+        top: '50%',
+         left: '50%',
+        transform: 'translate(-50%, -50%)'
+    }
+
     return (
         <header>
-            <img src="src/assets/react.svg" alt="" />
+            <div style={containerStyle}>
+                <img src="src/assets/forest.jpg" alt="" />
+                <div style={imageStyle}>Marko</div>
+            </div>
         </header>
     )
 }
